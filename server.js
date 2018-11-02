3efda278870c01cdbbfceaf63f2321d070fc5c45
zenott <35330692+zenott@users.noreply.github.com>
2018-11-02 13:28:50 +0100

    Add solution

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 'use strict';
 
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 
 // require and use "multer"...
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
-var app = express();
+const del = require('del');
+
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -17,6 +21,12 @@ app.get('/', function (req, res) {
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res, next){
+  res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+  del.sync([`uploads/**`, `!uploads`]);
+});
+
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
